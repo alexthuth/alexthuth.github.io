0eaf6cd8b8b801c4878916726d4ccb81e9470c47
Alex Huth <alexander.t.huth@gmail.com>
2023-11-15 15:57:11 -0800

    Added a transition in for content

--- navbar/ele_navbar.js
+++ navbar/ele_navbar.js
@@ -1,3 +1,12 @@
 fetch('navbar/ele_navbar.html')
     .then(response => response.text())
-    .then(data => document.getElementById('ele_navbar').innerHTML = data);
+    .then(data => {
+            document.getElementById('ele_navbar').innerHTML = data;
+            document.getElementById('preloader').style.display = 'none';
+            let navbarAndContent = document.querySelector('.navbar-and-content');
+            navbarAndContent.classList.add('flex-layout');
+            navbarAndContent.style.opacity = 1;
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    })
